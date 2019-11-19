9b5e2bea463e220849e25513ed0d44564c640fe2
cookiebaker444 <43713840+cookiebaker444@users.noreply.github.com>
2019-11-18 17:31:47 -0800

    Add files via upload

--- homedistancecal.py
+++ homedistancecal.py
@@ -8,6 +8,7 @@ os.getcwd()
 os.chdir('D:/D drive/CMPE 272/project')
 df = pd.read_csv('housingData.csv',header = None,error_bad_lines=False)
 df2 = pd.read_csv('public transport address process.csv',header = None,error_bad_lines=False)
+df3 = pd.read_csv('whole foods address process.csv', header = None, error_bad_lines = False)
 df['shortest distance'] = 0
 h = []
 for i in range(1, df.shape[0]):
@@ -20,4 +21,16 @@ for i in range(1, df.shape[0]):
         heapq.heappush(h, distance)
     df.iloc[i,9] = heapq.heappop(h)
 df.iloc[0,9] = 'shortest distance to public trans'
+df['shortest distance 2'] = 0
+h = []
+for i in range(1, df.shape[0]):
+    for j in range(1, df3.shape[0]):
+        lats = float(df.iloc[i, 4])
+        late = float(df3.iloc[j, 1])
+        lngs = float(df.iloc[i,5])
+        lnge = float(df3.iloc[j,2])
+        distance = math.acos(math.sin(math.pi*lats/180.0)*math.sin(math.pi*late/180.0)+math.cos(math.pi*lats/180.0)*math.cos(math.pi*late/180.0)*math.cos(math.pi*lngs/180.0-math.pi*lnge/180.0))*3963
+        heapq.heappush(h, distance)
+    df.iloc[i,10] = heapq.heappop(h)
+df.iloc[0,10] = 'shortest distance to whole foods'
 df.to_csv('housing data with distance to transportation.csv', header = False, index = False)
