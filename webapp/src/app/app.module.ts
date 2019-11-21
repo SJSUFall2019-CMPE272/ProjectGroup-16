c0317794598fc34cced00e7a611573c5d7ff9271
Mandy Wong <manyumandy.wong@sjsu.edu>
2019-11-21 01:34:34 -0800

    Merged firebase authenication with angular project

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginPage } from './loginPage/login.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { AnswerpageComponent } from './answerpage/answerpage.component';
 import { AnswerlistService } from './answerlist.service';
@@ -13,12 +14,13 @@ import { AnswerlistpageComponent } from './answerlistpage/answerlistpage.compone
   imports:      [ BrowserModule,
   ReactiveFormsModule,
   RouterModule.forRoot([
-    {path:'',component:AnswerpageComponent},
+    {path: '', component: LoginPage},
+    {path:'answer',component:AnswerpageComponent},
     {path:'answerlistpage',component:AnswerlistpageComponent}
   ])
   ],
 
-  declarations: [ AppComponent, 
+  declarations: [ AppComponent, LoginPage,
   TopbarComponent, AnswerpageComponent, AnswerlistpageComponent ],
   bootstrap:    [ AppComponent ],
   providers: [AnswerlistService]
