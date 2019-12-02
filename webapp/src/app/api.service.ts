cb3038acbfb7f2fe92389c8eb45db4ed73073a02
Matt Brown <mdbrown7079@gmail.com>
2019-12-01 21:41:07 -0800

    deploy the api to AWS and switch webapp over to the new API

--- webapp/src/app/api.service.ts
+++ webapp/src/app/api.service.ts
@@ -10,7 +10,7 @@ export class ApiService {
   }
 
   getResults(filters) {
-    return this.http.get(`http://localhost:5000/houses?${Object.keys(filters).map(key => 'check' + key[0].toUpperCase() + key.slice(1) + '=' + filters[key]).join('&')}`);
+    return this.http.get(`https://i0nisyp0el.execute-api.us-west-1.amazonaws.com/dev/houses?${Object.keys(filters).map(key => 'check' + key[0].toUpperCase() + key.slice(1) + '=' + filters[key]).join('&')}`);
   }
 
 }
